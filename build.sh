1f6a76bff3e1be63c24bc4dc5f939560b96cdc50
naok-000 <naotaro.kobayashi.000@gmail.com>
2026-01-18 01:06:13 +0900

    feat: copy my keymap from default

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ln -s $(pwd)/qmk_firmware/keyboards/keyball ../__qmk__/keyboards/keyball
-qmk compile -j 4 -kb keyball/keyball39 -km default
+qmk compile -j 4 -kb keyball/keyball39 -km my_keymap
 mkdir -p tmp
-cp ../__qmk__/keyball_keyball39_default.hex tmp/keyball39.hex
+cp ../__qmk__/keyball_keyball39_my_keymap.hex tmp/keyball39.hex
