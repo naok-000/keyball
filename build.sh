c6f5984bd56f6ec9dac73ae73cbc50fe34a249aa
naok-000 <naotaro.kobayashi.000@gmail.com>
2026-04-06 00:42:35 +0900

    ci: Add my_keymap to CI and update build directory structure

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ln -s $(pwd)/qmk_firmware/keyboards/keyball ../__qmk__/keyboards/keyball
-qmk compile -j 4 -kb keyball/keyball39 -km my_keymap
-mkdir -p tmp
-cp ../__qmk__/keyball_keyball39_my_keymap.hex tmp/keyball39.hex
+qmk compile -j 1 -kb keyball/keyball39 -km my_keymap
+mkdir -p build
+cp ../__qmk__/keyball_keyball39_my_keymap.hex build/keyball39.hex
