18a30ee52520778d5a8b20e471da0dd4af8b2b64
naok-000 <naotaro.kobayashi.000@gmail.com>
2026-04-06 00:55:06 +0900

    refactor: Update default keymap and enable VIA support

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ln -s $(pwd)/qmk_firmware/keyboards/keyball ../__qmk__/keyboards/keyball
-qmk compile -j 1 -kb keyball/keyball39 -km my_keymap
+qmk compile -j 1 -kb keyball/keyball39 -km default
 mkdir -p build
 cp ../__qmk__/keyball_keyball39_my_keymap.hex build/keyball39.hex
--- qmk_firmware/keyboards/keyball/keyball39/keymaps/default/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/default/config.h
@@ -21,19 +21,41 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_EFFECT_BREATHING
-#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
-#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-#    define RGBLIGHT_EFFECT_SNAKE
-#    define RGBLIGHT_EFFECT_KNIGHT
-#    define RGBLIGHT_EFFECT_CHRISTMAS
+//#    define RGBLIGHT_EFFECT_BREATHING
+//#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+//#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//#    define RGBLIGHT_EFFECT_SNAKE
+//#    define RGBLIGHT_EFFECT_KNIGHT
+//#    define RGBLIGHT_EFFECT_CHRISTMAS
 #    define RGBLIGHT_EFFECT_STATIC_GRADIENT
-#    define RGBLIGHT_EFFECT_RGB_TEST
-#    define RGBLIGHT_EFFECT_ALTERNATING
-#    define RGBLIGHT_EFFECT_TWINKLE
+//#    define RGBLIGHT_EFFECT_RGB_TEST
+//#    define RGBLIGHT_EFFECT_ALTERNATING
+//#    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
 #define TAP_CODE_DELAY 5
 
+// Enable automatic mouse layer feature
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 1
+#define AUTO_MOUSE_DEFAULT_LAYER 2
+#define AUTO_MOUSE_TIME 400
+
+// Define the number of layers
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+
+// Define tap and hold duration
+#define TAPPING_TERM 100
+
+// Make the hold/tap key easier to be recognized as a hold action.
+#define PERMISSIVE_HOLD
+// #define HOLD_ON_OTHER_KEY_PRESS
+
+// keyball unique configurations 
+// Set default CPI for the trackball
+#define KEYBALL_CPI_DEFAULT 1400
+
+// Set default scroll divider
+#define KEYBALL_SCROLL_DIV_DEFAULT 6
+
+// Enable scroll snap feature
+#define KEYBALL_SCROLLSNAP_ENABLE 2
